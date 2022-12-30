1151811c5def3bfc42b029c5f78fed1b80bc56ca
maximdudai <maxim.dudai01@gmail.com>
2022-12-30 01:12:44 +0000

    finished footer section with contact form, office and contact address
    full responsive for mobile/tablet/laptop/desktop screens

--- script/navbar/navbar.js
+++ script/navbar/navbar.js
@@ -17,14 +17,14 @@ mobileNavBarBtn_close.addEventListener("click", displayUserNavBar);
 // change navbar bg when scrolling down / up
 const navBarElement = document.querySelector(".navBarContainer");
 
-window.addEventListener('scroll', function(e){
-    // Get the new Value
-    const newValue = window.pageYOffset;
+// window.addEventListener('scroll', function(e){
+//     // Get the new Value
+//     const newValue = window.pageYOffset;
 
-    if(newValue) {
-        navBarElement.style.background = "#305ba1";
-    } else {
-        navBarElement.style = "background: linear-gradient( rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1) );";
+//     if(newValue) {
+//         navBarElement.style.background = "#305ba1";
+//     } else {
+//         navBarElement.style = "background: linear-gradient( rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1) );";
 
-    }
-});
+//     }
+// });
